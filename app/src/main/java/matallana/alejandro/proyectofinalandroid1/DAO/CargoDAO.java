89b7e573cde1f10f52d9090b8fc1b653e914f4bb
Alejandro <alejito.matallana@gmail.com>
2017-05-09 23:59:28 -0500

    Comit del dao Cargo Editar Y Eliminar

--- app/src/main/java/matallana/alejandro/proyectofinalandroid1/DAO/CargoDAO.java
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/DAO/CargoDAO.java
@@ -1,6 +1,7 @@
 package matallana.alejandro.proyectofinalandroid1.DAO;
 
 import android.app.Activity;
+import android.content.ContentValues;
 
 import matallana.alejandro.proyectofinalandroid1.Infraestructura.Conexion;
 import matallana.alejandro.proyectofinalandroid1.Modelo.Cargo;
@@ -21,15 +22,35 @@ public class CargoDAO {
         return true;
     }
 
+    /**
+     * Metodo para modificar un cargo
+     * @param cargo
+     * @return
+     */
     public boolean modificar(Cargo cargo) {
-        return true;
+        String tabla = "Cargos" ;
+        String condicion = "nombre=" +  cargo.getNombre();
+        ContentValues registro = new ContentValues();
+        registro.put("descripcion", cargo.getDescripcion());
+        registro.put("horario", cargo.getHorario());
+        registro.put("salario",cargo.getSalario());
+        registro.put("idProyecto", cargo.getIdProyecto().getNombre()); // Aca va la foranea del id del proyecto
+        return  conex.update(tabla,condicion,registro);
+
     }
 
     public Cargo buscar(String nombreCargo) {
         return null;
     }
 
-    public boolean eliminar(Cargo usuario) {
-        return true;
+    /**
+     * Metodo para eliminar un cargo
+     * @param cargo
+     * @return
+     */
+    public boolean eliminar(Cargo cargo) {
+        String tabla = "Cargos" ;
+        String condicion = "nombre=" +  cargo.getNombre();
+        return conex.delete(tabla,condicion);
     }
 }
