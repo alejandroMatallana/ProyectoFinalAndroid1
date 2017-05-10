8f264d384db81b6e3e1f5d9a9ddbbc38e48c40e3
miguel <migu3l1991@hotmail.com>
2017-05-10 11:20:56 -0500

    Arreglo de instancias de componentes

--- app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/CargoActivity.java
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/CargoActivity.java
@@ -2,14 +2,24 @@ package matallana.alejandro.proyectofinalandroid1.Vista;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import matallana.alejandro.proyectofinalandroid1.R;
 
 public class CargoActivity extends AppCompatActivity {
 
+    EditText txtNombre;
+    EditText txtHorario;
+    EditText txtSalario;
+    EditText txtDescripcion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cargo);
+        txtNombre = (EditText)findViewById(R.id.nombre);
+        txtHorario = (EditText)findViewById(R.id.horario);
+        txtSalario = (EditText)findViewById(R.id.salario);
+        txtDescripcion = (EditText)findViewById(R.id.descripcion);
     }
 }
--- app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/IntegranteActivity.java
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/IntegranteActivity.java
@@ -8,18 +8,9 @@ import matallana.alejandro.proyectofinalandroid1.R;
 
 public class IntegranteActivity extends AppCompatActivity {
 
-    EditText txtNombre;
-    EditText txtHorario;
-    EditText txtSalario;
-    EditText txtDescripcion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_integrante);
-        txtNombre = (EditText)findViewById(R.id.nombre);
-        txtHorario = (EditText)findViewById(R.id.horario);
-        txtSalario = (EditText)findViewById(R.id.salario);
-        txtDescripcion = (EditText)findViewById(R.id.descripcion);
     }
 }
