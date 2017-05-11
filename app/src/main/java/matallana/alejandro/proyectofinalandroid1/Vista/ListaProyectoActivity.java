3cf4195718b3a0c2e3d95c7dadf1ea2897e199f2
sebastiancardona <krdona-k44@hotmail.com>
2017-05-10 19:30:49 -0500

    avance gestion proyectos

--- app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/ListaProyectoActivity.java
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/ListaProyectoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,12 +17,19 @@ import matallana.alejandro.proyectofinalandroid1.R;
 public class ListaProyectoActivity extends AppCompatActivity {
 
     private ListView lstProyectos;
+    private ArrayList<Proyecto> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_proyecto);
         lstProyectos = (ListView) findViewById(R.id.lstListaProyectos);
+        cargarLista();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         cargarLista();
     }
 
@@ -33,9 +41,18 @@ public class ListaProyectoActivity extends AppCompatActivity {
 
     public void cargarLista(){
         ControllerProyecto controlador = new ControllerProyecto(this);
-        ArrayList<Proyecto> lista = controlador.listarProyectos();
+        lista = controlador.listarProyectos();
         ArrayAdapter<Proyecto> adapter = new ArrayAdapter<Proyecto>(this,android.R.layout.simple_list_item_1,lista);
         lstProyectos.setAdapter(adapter);
+        lstProyectos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Proyecto proyectoSeleccionado = lista.get(position);
+                MenuProyectosActivity.proyecto = proyectoSeleccionado;
+                Intent i = new Intent(getApplicationContext(),MenuProyectosActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
