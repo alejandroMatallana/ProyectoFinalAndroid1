03f2de829f419c62c28016e7c3fe5cb09e2dd445
Alejandro <alejito.matallana@gmail.com>
2017-05-16 01:04:10 -0500

    Menu actividades con variable estatica

---
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/MenuActividadesActivity.java
@@ -0,0 +1,19 @@
+package matallana.alejandro.proyectofinalandroid1.Vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import matallana.alejandro.proyectofinalandroid1.Modelo.Actividad;
+import matallana.alejandro.proyectofinalandroid1.R;
+
+public class MenuActividadesActivity extends AppCompatActivity {
+
+
+    public static Actividad tipoActividad=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_actividades);
+    }
+}
