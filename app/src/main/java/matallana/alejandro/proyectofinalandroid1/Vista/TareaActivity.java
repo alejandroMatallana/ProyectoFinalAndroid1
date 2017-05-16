0713d4abc1c181ebaa01b16f0443d5bf150d6aca
miguel <migu3l1991@hotmail.com>
2017-05-16 13:06:05 -0500

    Cambios en Acivity Tarea

--- app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/TareaActivity.java
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Vista/TareaActivity.java
@@ -1,20 +1,20 @@
 package matallana.alejandro.proyectofinalandroid1.Vista;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import matallana.alejandro.proyectofinalandroid1.Controlador.ControllerTarea;
-import matallana.alejandro.proyectofinalandroid1.Modelo.Tarea;
 import matallana.alejandro.proyectofinalandroid1.R;
 
 public class TareaActivity extends AppCompatActivity {
 
     public static int tipo = 0;
     private ControllerTarea controllerTarea;
-    private EditText nombre, procentaje, fechaIni, fechaFin;
+    private EditText nombre, porcentaje, fechaIni, fechaFin;
     private Button crear, editar, eliminar;
 
     @Override
@@ -23,7 +23,7 @@ public class TareaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tarea);
         controllerTarea = new ControllerTarea(this);
         nombre = (EditText) findViewById(R.id.nombreTarea);
-        procentaje = (EditText) findViewById(R.id.porcentajeTarea);
+        porcentaje = (EditText) findViewById(R.id.porcentajeTarea);
         fechaIni = (EditText) findViewById(R.id.fechaIni);
         fechaFin = (EditText) findViewById(R.id.fechaFin);
         crear = (Button) findViewById(R.id.botonCrear);
@@ -44,7 +44,13 @@ public class TareaActivity extends AppCompatActivity {
     }
 
     public void crear(View view) {
-        limpiar();
+        if (nombre.getText().toString().isEmpty() || porcentaje.getText().toString().isEmpty() ||
+                fechaIni.getText().toString().isEmpty() || fechaFin.getText().toString().isEmpty()) {
+            Toast.makeText(this,"Debe ingresar todos los datos",Toast.LENGTH_SHORT).show();
+        } else {
+
+            limpiar();
+        }
     }
 
     public void editar(View view) {
@@ -58,7 +64,7 @@ public class TareaActivity extends AppCompatActivity {
     public void limpiar() {
         tipo = 0;
         nombre.setText(null);
-        procentaje.setText(null);
+        porcentaje.setText(null);
         fechaIni.setText(null);
         fechaFin.setText(null);
     }
