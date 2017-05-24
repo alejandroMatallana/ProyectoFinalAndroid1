d496ef7387d56aec2cb1d1c9b14e46bbc909988f
sebastiancardona <krdona-k44@hotmail.com>
2017-05-24 15:05:03 -0500

    creacion de clases de comentario de las tareas

---
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/DAO/ComentarioDAO.java
@@ -0,0 +1,73 @@
+package matallana.alejandro.proyectofinalandroid1.DAO;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import matallana.alejandro.proyectofinalandroid1.Infraestructura.Conexion;
+import matallana.alejandro.proyectofinalandroid1.Modelo.ComentarioTarea;
+import matallana.alejandro.proyectofinalandroid1.Modelo.Tarea;
+
+/**
+ * Created by sebastiancardona on 24/05/17.
+ */
+
+public class ComentarioDAO {
+
+    private Conexion conex;
+
+    public ComentarioDAO(Activity activity) {
+        conex = new Conexion(activity);
+    }
+
+    /**
+     * metodo para guardar un comentario de una tarea
+     * @param comentario, el comentario que será guardado
+     * @return true si el comentario se registro corectamente, false de lo contrario
+     */
+    public boolean guardar(ComentarioTarea comentario) {
+        ContentValues registro= new ContentValues();
+        Date fecha = comentario.getFecha();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+        registro.put("fecha", format.format(fecha));
+        registro.put("titulo", comentario.getTitulo());
+        registro.put("comentario", comentario.getComentario());
+        registro.put("idTarea", comentario.getTarea().getId());
+        return conex.insert("ComentarioTareas", registro);
+    }
+
+    /**
+     * metodo para listar los comentarios de una tarea
+     * @param tarea, la tarea
+     * @return la lista de comentarios que se han registrado de esa tarea
+     */
+    public ArrayList<ComentarioTarea> listar(Tarea tarea){
+        ArrayList<ComentarioTarea> lista = new ArrayList<>();
+        String consulta = "SELECT ct.id, ct.fecha, ct.titulo, ct.comentario" +
+                " FROM ComentarioTareas AS ct WHERE ct.idTarea="+tarea.getId() + " ORDER BY fecha";
+        Cursor temp = conex.search(consulta);
+        if (temp.moveToFirst()){
+            do {
+                Date fecha = null;
+                SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+                try {
+                    fecha = format.parse(temp.getString(1));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                ComentarioTarea comentario = new ComentarioTarea(temp.getInt(0),fecha,
+                        temp.getString(2),temp.getString(3),tarea);
+
+                lista.add(comentario);
+
+            } while (temp.moveToNext());
+        }
+        return lista;
+    }
+}
---
+++ app/src/main/java/matallana/alejandro/proyectofinalandroid1/Modelo/ComentarioTarea.java
@@ -0,0 +1,104 @@
+package matallana.alejandro.proyectofinalandroid1.Modelo;
+
+import java.util.Date;
+
+/**
+ * Created by sebastiancardona on 24/05/17.
+ */
+
+public class ComentarioTarea {
+
+    private int id;
+    /**
+     * fecha en la que se escribio el comentario
+     */
+    private Date fecha;
+    private String titulo;
+    private String comentario;
+    /**
+     * tarea a la que pertence este comentario
+     */
+    private Tarea tarea;
+
+    /**
+     * Constructor vacio...
+     */
+    public ComentarioTarea() {
+        super();
+    }
+
+    /**
+     * constructor sin id...
+     * @param fecha
+     * @param titulo
+     * @param comentario
+     */
+    public ComentarioTarea(Date fecha, String titulo, String comentario, Tarea tarea) {
+        this.fecha = fecha;
+        this.titulo = titulo;
+        this.comentario = comentario;
+        this.tarea = tarea;
+    }
+
+    /**
+     * Constructor con id...
+     * @param id
+     * @param fecha
+     * @param titulo
+     * @param comentario
+     */
+    public ComentarioTarea(int id, Date fecha, String titulo, String comentario, Tarea tarea) {
+        this.id = id;
+        this.fecha = fecha;
+        this.titulo = titulo;
+        this.comentario = comentario;
+        this.tarea = tarea;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Tarea getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+    @Override
+    public String toString() {
+        return "Fecha de publicación: " + fecha
+                + "\nTitulo: "+ titulo
+                + "\nComentario: "+ comentario;
+    }
+}
